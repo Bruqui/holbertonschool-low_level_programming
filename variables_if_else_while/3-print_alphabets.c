4a386522827ed8efe6477f6f3ee409eb250f749a
root <dylan2003gd@gmail.com>
2024-06-18 11:02:08 +0000

    3. alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int c = 0;
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+		for (c = 'A'; c <= 'Z'; c++)
+                        putchar(c);
+		putchar('\n');
+		return (0);
+	}
+}
