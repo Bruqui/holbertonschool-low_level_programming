664fce78a0ee34c4bd775a7dba150b4a7f4b29ef
root <dylan2003gd@gmail.com>
2024-06-24 13:34:07 +0000

    10. Triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,13 +5,14 @@ void print_triangle(int size)
 	int i;
 	int j;
 
-	for (i = 0; i <= size; i++)
-	{
-		for (j = size - i; j > 0; j--)
-				_putchar(' ');
-		for (j = 0; j < i; j++)
-				_putchar('#');
-		_putchar('\n');
-	}
+	if (size > 0)
+		for (i = 0; i <= size; i++)
+		{	
+			for (j = size - i; j > 0; j--)
+					_putchar(' ');
+			for (j = 0; j < i; j++)
+					_putchar('#');
+			_putchar('\n');
+		}
 	_putchar('\n');
 }
