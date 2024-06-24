34998d0392c87c740650968ecd7628c220223b2e
root <dylan2003gd@gmail.com>
2024-06-24 11:27:08 +0000

    4. I believe in numbers and signs

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,8 @@ void print_most_numbers(void)
 	int i = 0;
 
 	for (; i < 10; i++)
-		if (i != '2' && i != '4')
-			_putchar(i);
+		if (i == 2 || i == 4)
+			i++;
+		_putchar(i + '0');
 	_putchar('\n');
 }
