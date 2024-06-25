8c85e06c8585207d6dc4447366c458e379707af9
root <dylan2003gd@gmail.com>
2024-06-25 11:57:35 +0000

    4. I can only go one way. I've not got a reverse gear

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		while (i>= 0)
+		{
+			_putchar(str[i]);
+			i--;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
