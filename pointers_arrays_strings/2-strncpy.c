af038a5bcb4dc6c4dba8f413b50dfb252fb47da0
root <dylan2003gd@gmail.com>
2024-06-28 10:08:23 +0000

    2. strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strncpy - copies a string of n bytes
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes from src string
+ * Return: dest
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
