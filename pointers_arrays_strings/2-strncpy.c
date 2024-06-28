52cb6946c11f37a825782950645da4b0bea788cd
root <dylan2003gd@gmail.com>
2024-06-28 09:58:01 +0000

    2. strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,9 +4,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && n > 0; i++, n--)
+	for (i = 0; src[i] != '\0' && i < n; i++, n--)
+	{
 		dest[i] = src[i];
-	for (i = 0; i < n; i++)
 		dest[i] = '\0';
+	}
 	return (dest);
 }
