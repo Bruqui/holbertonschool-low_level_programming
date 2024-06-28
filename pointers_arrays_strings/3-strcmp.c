13af494d9302030a3ff2e0d9ed1e7d3bd9d8d2bb
root <dylan2003gd@gmail.com>
2024-06-28 10:53:38 +0000

    3. strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _strcmp - compare 2 strings
+ * @s1: 1st string to compare
+ * @s2: 2nd string to compare
+ * Return: *s1 - *s2 or 0
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 != '\0' && (*s1 == *s2))
