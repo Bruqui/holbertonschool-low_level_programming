58111e2033852d793525f7a5f82a8431ff5bf658
root <dylan2003gd@gmail.com>
2024-06-28 08:45:58 +0000

    0. strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	while (*dest[i] != '\0')
+		i++;
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[i] + src [j];
+	dest[i + j] = '\0';
+	return (dest);
+}
