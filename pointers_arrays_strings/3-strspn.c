80847cbab9baba9a5f03b5cec9dc5a0f4ada0911
root <dylan2003gd@gmail.com>
2024-07-01 12:04:37 +0000

    3. strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: string to test
+ * @accept: characters allowed
+ * Return: number of bytes of inital segment of s,
+ * consist only of bytes form accept
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i;
