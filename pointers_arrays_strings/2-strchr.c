e8beb547752cb1b27bcaa3f031633bfaeb344164
root <dylan2003gd@gmail.com>
2024-07-01 10:49:22 +0000

    2. strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+  * _strchr - main function
+  * @s: Function parameter
+  * @c: Function parameter
+  * Return: Always o.
+  */
+
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
