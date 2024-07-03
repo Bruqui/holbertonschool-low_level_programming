1342d9af89394a12f675a99a3f1ead68b822d966
root <dylan2003gd@gmail.com>
2024-07-03 08:44:11 +0000

    0. It ain't what they call you, it's what you answer to

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: number of agument
+ * @argv: array of arguments
+ * Return: 0 seccess
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
