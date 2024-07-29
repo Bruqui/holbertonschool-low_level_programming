fef53d4138061ee314b15f804c3bc0d64e7bdfee
root <dylan2003gd@gmail.com>
2024-07-29 12:14:18 +0000

    5. The Hare and the Tortoise

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * pre_main - Prints a message before the main function executes.
  */
 
-void pre_main(void) __attribute__ ((constructor))
+void __attribute__ ((constructor)) pre_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
