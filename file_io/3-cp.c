f1d9a03151af7a5ab56356914b031d500cf7ace9
root <dylan2003gd@gmail.com>
2024-08-02 09:53:00 +0000

    3. cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,69 +1,42 @@
 #include "main.h"
 
 /**
- * fileError - check if have a error
- * @file_from: start file
- * @file_to: destination file
- * @argv: list arg
- */
-
-void fileError(int file_from, int file_to, char *argv[])
-{
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
-
-/**
- * main - check the code
- * @argc: Number of arguments
- * @argv: Argument vector
- * Return: Always 0.
- */
+  * main - copies the content of a file to another file
+  * @argc: number of arguments
+  * @argv: array of arguments passed to function
+  * Return: 0 on success or exit with error code on failure
+  */
 
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, re;
-	ssize_t i, wr;
-	char buf[1024];
+	int og_fd, cp_fd, read_count, write_count;
+	char buffer[1024];
+	mode_t permission =  0000200 | S_IRUSR | S_IWGRP | S_IROTH | S_IRGRP;
 
 	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	og_fd = open(argv[1], O_RDONLY);
+	if (og_fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	cp_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, permission);
+	if (cp_fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	read_count = 1;
+	while (read_count)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		read_count = read(og_fd, buffer, 1024);
+		if (read_count == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		if (read_count > 0)
+		{
+			write_count = write(cp_fd, buffer, read_count);
+			if (write_count != read_count || write_count == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		}
 	}
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	fileError(file_from, file_to, argv);
-
-	i = 1024;
-	while (i == 1024)
-	{
-		i = read(file_from, buf, 1024);
-		if (i == -1)
-			fileError(-1, 0, argv);
-		wr = write(file_to, buf, i);
-		if (wr == -1)
-			fileError(0, -1, argv);
-	}
-	re = close(file_from);
-	if (re == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	re = close(file_to);
-	if (re == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+	if (close(og_fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", og_fd), exit(100);
+	if (close(cp_fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_fd), exit(100);
 	return (0);
 }
